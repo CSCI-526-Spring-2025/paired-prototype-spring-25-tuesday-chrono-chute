5e8c1658209e763b02ed2d4bde6e64d2a7d08552
Srivarshan <srivarshan02@gmail.com>
2025-02-17 12:31:27 -0800

    Add code to display remaining switches

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -1,26 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Switch : MonoBehaviour
 {
     public bool isPresent;
     public float switchAmount = 20.0f;
+    public int maxSwitches = 3; // Maximum number of switches allowed
+    private int switchesRemaining; // Track remaining switches
+    public TMP_Text switchCountText; // Reference to the UI text
 
     // Start is called before the first frame update
     void Start()
     {
         isPresent = true;
-
+        switchesRemaining = maxSwitches;
+        UpdateSwitchDisplay();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && switchesRemaining > 0)
         {
             transform.Translate(Vector3.right * (isPresent ? -switchAmount : switchAmount));
             isPresent = !isPresent;
+            switchesRemaining--;
+            UpdateSwitchDisplay();
+            Debug.Log($"Switches remaining: {switchesRemaining}");
+        }
+    }
+
+    void UpdateSwitchDisplay()
+    {
+        if (switchCountText != null)
+        {
+            switchCountText.text = $"Switches Left: {switchesRemaining}";
         }
     }
 }
